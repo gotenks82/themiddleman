01b3f84d0cec492de4d2eaeab1f32f693d193b94
Alex Pedini <alex.pedini@gmail.com>
2018-10-21 07:07:38 +0200

    * small cleanup and milestone update

--- src/main/kotlin/com/eurohack/middleman/models/ChatMessage.kt
+++ src/main/kotlin/com/eurohack/middleman/models/ChatMessage.kt
@@ -1,11 +1,9 @@
 package com.eurohack.middleman.models
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.Instant
 
 data class ChatMessage(
         val from: String,
         val content: String,
-        @JsonFormat()
         val timestamp: Instant = Instant.now()
 )
